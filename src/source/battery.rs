c3000be821008a1d8dd52816aa9f032a3e7061ae
Hailey Somerville <hailey@hailey.lol>
2022-07-19 19:52:24 +1000

    battery: factor out stream fn

--- src/source/battery.rs
+++ src/source/battery.rs
@@ -5,18 +5,11 @@ use crate::util::file_contents;
 use crate::util::stream::{combine, dedup};
 
 pub fn battery() -> impl Stream<Item = Option<String>> {
-    let charge_now = file_contents::strings(&Path::new("/sys/class/power_supply/BAT0/charge_now"));
-    let charge_full = file_contents::strings(&Path::new("/sys/class/power_supply/BAT0/charge_full"));
+    let charge_now = stream(Path::new("/sys/class/power_supply/BAT0/charge_now"));
+    let charge_full = stream(Path::new("/sys/class/power_supply/BAT0/charge_full"));
 
     let stream = combine(charge_now, charge_full)
         .map(|(now, full)| {
-            fn parse(opt: Option<String>) -> Option<i32> {
-                opt.and_then(|val| val.trim().parse().ok())
-            }
-
-            let now = parse(now);
-            let full = parse(full);
-
             now.zip(full).map(|(now, full)| {
                 let percent = now * 100 / full;
                 format!("{}%", percent)
@@ -24,4 +17,9 @@ pub fn battery() -> impl Stream<Item = Option<String>> {
         });
 
     dedup(stream)
+}
+
+fn stream(path: &Path) -> impl Stream<Item = Option<i32>> {
+    file_contents::strings(path)
+        .map(|string| string?.trim().parse().ok())
 }
